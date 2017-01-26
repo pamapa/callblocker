3f7a4fca068747c2fcc83db98112aac4f617e47a
pamapa <pamapa@users.noreply.github.com>
2017-01-26 21:20:46 +0100

    fix online lookup via www.dasschnelle.at

--- usr/share/callblocker/onlinelookup_dasschnelle_at.py
+++ usr/share/callblocker/onlinelookup_dasschnelle_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # callblocker - blocking unwanted calls from your home phone
-# Copyright (C) 2015-2016 Patrick Ammann <pammann@gmx.net>
+# Copyright (C) 2015-2017 Patrick Ammann <pammann@gmx.net>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
