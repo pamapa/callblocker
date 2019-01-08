b97e7cebeac0edae5d69e3c98c4bfc5c0ac66d60
pamapa <pamapa@users.noreply.github.com>
2019-01-08 20:39:53 +0100

    update version

--- usr/var/www/callblocker/js/app.js
+++ usr/var/www/callblocker/js/app.js
@@ -901,7 +901,7 @@ require(["dijit/ConfirmDialog",
   var statusbarPane = new dijit.layout.ContentPane({
     region: "bottom",
     style: "text-align: center",
-    content: "v0.11.1"
+    content: "v0.12.0"
   });
   appLayout.addChild(statusbarPane);
   appLayout.placeAt(document.body);
