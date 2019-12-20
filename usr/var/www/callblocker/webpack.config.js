7ea8a9d86ee9f90405099b9f341f7139540ea85b
pamapa <pamapa@users.noreply.github.com>
2019-12-20 13:43:39 +0100

    update npm dev packages

--- usr/var/www/callblocker/webpack.config.js
+++ usr/var/www/callblocker/webpack.config.js
@@ -17,84 +17,79 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-var path = require("path");
-var webpack = require("webpack");
+const path = require("path");
+const webpack = require("webpack");
 
-var DojoWebpackPlugin = require("dojo-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const DojoWebpackPlugin = require("dojo-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isDevMode = !!(env||{}).dev;
-	return {
-		context: __dirname,
-		entry: "src/bootstrap",
-		output: {
-			path: path.join(__dirname, "dist"),
-			publicPath: "dist/",
-			pathinfo: true,
-			filename: "bundle.js"
-		},
-		module: {
-			rules: [{
-		    test: /\.(png)|(gif)$/,
-		    use: [
-		      {
-		        loader: 'url-loader',
-		        options: {
-		          limit: 100000
-		        }
-		      }
-		    ]
-		  }]
-		},
-		plugins: [
+  return {
+    context: __dirname,
+    entry: "src/bootstrap",
+    output: {
+      path: path.join(__dirname, "dist"),
+      publicPath: "dist/",
+      pathinfo: true,
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [{
+        test: /\.(png)|(gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      }]
+    },
+    plugins: [
       // JavaScript
-			new DojoWebpackPlugin({
-				loaderConfig: require("./src/loaderConfig"),
-				environment: {dojoRoot: "dist"},	// used at run time for non-packed resources (e.g. blank.gif)
-				buildEnvironment: {dojoRoot: "node_modules"}, // used at build time
-				locales: ["en"],
-				noConsole: true
-			}),
+      new DojoWebpackPlugin({
+        loaderConfig: require("./src/loaderConfig"),
+        environment: {dojoRoot: "dist"},  // used at run time for non-packed resources (e.g. blank.gif)
+        buildEnvironment: {dojoRoot: "node_modules"}, // used at build time
+        locales: ["en"],
+        noConsole: true
+      }),
 
-			// Copy non-packed resources needed by the app to the release directory
-			new CopyWebpackPlugin([{
-				context: "node_modules",
-				from: "dojo/resources/blank.gif",
-				to: "dojo/resources"
-			}]),
+      // Copy non-packed resources needed by the app to the release directory
+      new CopyWebpackPlugin([{
+        context: "node_modules",
+        from: "dojo/resources/blank.gif",
+        to: "dojo/resources"
+      }]),
 
-			// For plugins registered after the dojo-webpack-plugin, data.request has been normalized and
-			// resolved to an absMid and loader-config maps and aliases have been applied
-			new webpack.NormalModuleReplacementPlugin(/^dojox\/gfx\/renderer!/, "dojox/gfx/canvas"),
-			new webpack.NormalModuleReplacementPlugin(
-				/^css!/, function(data) {
-					data.request = data.request.replace(/^css!/, "!style-loader!css-loader!less-loader!")
-				}
-			)
-		],
-		resolveLoader: {
-			modules: ["node_modules"]
-		},
-		mode: isDevMode ? 'development' : 'production',
-		optimization: {
-			namedModules: false,
-			splitChunks: false,
-			minimizer: isDevMode ? [] : [
-	      new UglifyJsPlugin({
-	        cache: true,
-	        parallel: true,
-	        uglifyOptions: {
-	          compress: true,
-	          mangle: true,
-						output: {comments: false}
-	        },
-	        sourceMap: false
-	      })
-	    ],
-	  },
+      // For plugins registered after the dojo-webpack-plugin, data.request has been normalized and
+      // resolved to an absMid and loader-config maps and aliases have been applied
+      new webpack.NormalModuleReplacementPlugin(/^dojox\/gfx\/renderer!/, "dojox/gfx/canvas"),
+      new webpack.NormalModuleReplacementPlugin(
+        /^css!/, function(data) {
+          data.request = data.request.replace(/^css!/, "!style-loader!css-loader!less-loader!")
+        }
+      )
+    ],
+    resolveLoader: {
+      modules: ["node_modules"]
+    },
+    mode: isDevMode ? 'development' : 'production',
+    optimization: {
+      namedModules: false,
+      splitChunks: false,
+      minimizer: isDevMode ? [] : [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false
+        })
+      ],
+    },
     performance: { hints: false },
-		devtool: "#source-map"
-	};
+    devtool: "#source-map"
+  };
 };
