634e2879e6927563a03f4e654b7282c84986e0c0
pamapa <pamapa@users.noreply.github.com>
2020-10-14 11:51:45 +0200

    feat: #70 bump copyright year

--- src/Notify.cpp
+++ src/Notify.cpp
@@ -1,6 +1,6 @@
 /*
  callblocker - blocking unwanted calls from your home phone
- Copyright (C) 2015-2019 Patrick Ammann <pammann@gmx.net>
+ Copyright (C) 2015-2020 Patrick Ammann <pammann@gmx.net>
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
--- usr/share/callblocker/import_CSV.py
+++ usr/share/callblocker/import_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # callblocker - blocking unwanted calls from your home phone
-# Copyright (C) 2015-2017 Patrick Ammann <pammann@gmx.net>
+# Copyright (C) 2015-2020 Patrick Ammann <pammann@gmx.net>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
