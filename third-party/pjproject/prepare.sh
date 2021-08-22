b040d317459bcaa89bf4c097a6d7b19f701b27c2
pamapa <pamapa@users.noreply.github.com>
2021-08-22 19:39:22 +0200

    Bump pjsip to v2.11.1

--- third-party/pjproject/prepare.sh
+++ third-party/pjproject/prepare.sh
@@ -3,10 +3,10 @@
 # https://github.com/asterisk/asterisk/blob/master/third-party/pjproject/Makefile.rules
 #
 
-TARBALL_FILE="2.11.tar.gz"
+TARBALL_FILE="2.11.1.tar.gz"
 TARBALL_MD5="$TARBALL_FILE.md5"
 PACKAGE_URL="https://github.com/pjsip/pjproject/archive/refs/tags/$TARBALL_FILE"
-UNPACK_DIR="pjproject-2.11"
+UNPACK_DIR="pjproject-2.11.1"
 
 # download
 if [ ! -f "$TARBALL_FILE" ]; then
